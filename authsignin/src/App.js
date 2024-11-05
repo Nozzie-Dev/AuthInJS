2c4c97fb821414bff138f751ba2fd41aed6aa0f4
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-11-05 12:20:39 +0200

    Update App.js resolve deploy error

--- authsignin/src/App.js
+++ authsignin/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SignUpPage from './components/Signup';
-import Login from './components/Login';
+import Login from './components/LogIn';
 import Profile from './components/Profile';
 
 function App() {
